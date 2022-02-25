9522780cc14a58a90c76586b3330d8030335f2fa
lemon-mint <ice.lemons.mint@gmail.com>
2022-02-25 23:30:02 +0900

    func (m Method) String() string

--- methods.go
+++ methods.go
@@ -21,3 +21,30 @@ const (
 	According to RFC7231 Section 4.1,
 	All general purpose HTTP/1.1 servers MUST support the GET, HEAD.
 */
+
+func (m Method) String() string {
+	switch m {
+	case MethodGET:
+		return "GET"
+	case MethodHEAD:
+		return "HEAD"
+	case MethodPOST:
+		return "POST"
+	case MethodPUT:
+		return "PUT"
+	case MethodDELETE:
+		return "DELETE"
+	case MethodCONNECT:
+		return "CONNECT"
+	case MethodOPTIONS:
+		return "OPTIONS"
+	case MethodTRACE:
+		return "TRACE"
+	case MethodPATCH:
+		return "PATCH"
+	case MethodBREW:
+		return "BREW"
+	default:
+		return "UNKNOWN"
+	}
+}
