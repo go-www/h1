0008b3a30c5c2f0a547591a55e790e03511ec591
lemon-mint <ice.lemons.mint@gmail.com>
2022-03-23 23:40:52 +0900

    Revert d42c1ac2c05be256d07598f86638dde5b7c68ff6

--- response.go
+++ response.go
@@ -10,7 +10,7 @@ var ResponsePool = sync.Pool{
 	New: func() any {
 		return &Response{
 			upstream:      nil,
-			buf:           make([]byte, 8192),
+			buf:           make([]byte, 0, 8192),
 			itoaBuf:       make([]byte, 0, 32),
 			n:             0,
 			ContentLength: -1,
@@ -36,7 +36,6 @@ type Response struct {
 
 	buf []byte // Note: Do not use append() to add bytes to this buffer. Use Write() instead. This is to avoid unnecessary memory allocations.
 	n   int
-	err error
 
 	// Itoa Buffer
 	itoaBuf []byte // buffer for itoa
@@ -48,6 +47,7 @@ type Response struct {
 
 func (r *Response) Reset() {
 	r.n = 0
+	r.buf = r.buf[:0]
 	r.ContentLength = -1
 }
 
@@ -62,117 +62,68 @@ var DateServerHeaderFunc = func() []byte {
 	return DefaultFastDateServer.GetDate()
 }
 
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the GOLICENSE file.
-
-// Package bufio implements buffered I/O. It wraps an io.Reader or io.Writer
-// object, creating another object (Reader or Writer) that also implements
-// the interface but provides buffering and some help for textual I/O.
-
-// Flush writes any buffered data to the underlying upstream.
 func (r *Response) Flush() error {
-	if r.err != nil {
-		return r.err
-	}
-	if r.n == 0 {
+	if r.upstream == nil || r.n == 0 {
 		return nil
 	}
-	n, err := r.upstream.Write(r.buf[0:r.n])
-	if n < r.n && err == nil {
-		err = io.ErrShortWrite
-	}
+
+	_, err := r.upstream.Write(r.buf[:r.n])
 	if err != nil {
-		if n > 0 && n < r.n {
-			copy(r.buf[0:r.n-n], r.buf[n:r.n])
-		}
-		r.n -= n
-		r.err = err
 		return err
 	}
-	r.n = 0
+
+	r.Reset()
 	return nil
 }
 
-// Available returns how many bytes are unused in the buffer.
-func (r *Response) Available() int {
-	return len(r.buf) - r.n
-}
-
-// AvailableBuffer returns an empty buffer with b.Available() capacity.
-// This buffer is intended to be appended to and
-// passed to an immediately succeeding Write call.
-// The buffer is only valid until the next write operation on b.
-func (r *Response) AvailableBuffer() []byte {
-	return r.buf[r.n:][:0]
-}
-
-// Buffered returns the number of bytes that have been written into the current buffer.
-func (r *Response) Buffered() int {
-	return r.n
-}
-
-// Write writes the contents of p into the buffer.
-// It returns the number of bytes written.
-// If nn < len(p), it also returns an error explaining
-// why the write is short.
-func (r *Response) Write(p []byte) (nn int, err error) {
-	for len(p) > r.Available() && r.err == nil {
-		var n int
-		if r.Buffered() == 0 {
-			// Large write, empty buffer.
-			// Write directly from p to avoid copy.
-			n, r.err = r.upstream.Write(p)
-		} else {
-			n = copy(r.buf[r.n:], p)
-			r.n += n
-			r.Flush()
-		}
-		nn += n
-		p = p[n:]
-	}
-	if r.err != nil {
-		return nn, r.err
-	}
-	n := copy(r.buf[r.n:], p)
+func (r *Response) Write(b []byte) (int, error) {
+	n := copy(r.buf[r.n:cap(r.buf)], b) // copy to buffer
 	r.n += n
-	nn += n
-	return nn, nil
+	if len(r.buf) < cap(r.buf) {
+		return n, nil
+	}
+
+	// buffer is full, flush it
+	err := r.Flush()
+	if err != nil {
+		return 0, err
+	}
+
+	// If b is bigger than buffer, write it directly
+	if len(b)-n > cap(r.buf) {
+		_, err = r.upstream.Write(b[n:])
+		return len(b), err
+	}
+
+	// copy b to buffer
+	copy(r.buf, b[n:])
+	r.n = len(b) - n
+	return n, nil
 }
 
-// WriteString writes a string.
-// It returns the number of bytes written.
-// If the count is less than len(s), it also returns an error explaining
-// why the write is short.
-func (r *Response) WriteString(s string) (int, error) {
-	nn := 0
-	for len(s) > r.Available() && r.err == nil {
-		n := copy(r.buf[r.n:], s)
-		r.n += n
-		nn += n
-		s = s[n:]
-		r.Flush()
-	}
-	if r.err != nil {
-		return nn, r.err
-	}
-	n := copy(r.buf[r.n:], s)
+func (r *Response) WriteString(b string) (int, error) {
+	n := copy(r.buf[r.n:cap(r.buf)], b) // copy to buffer
 	r.n += n
-	nn += n
-	return nn, nil
-}
+	if len(r.buf) < cap(r.buf) {
+		return n, nil
+	}
 
-// WriteByte writes a single byte.
-func (r *Response) WriteByte(c byte) error {
-	if r.err != nil {
-		return r.err
+	// buffer is full, flush it
+	err := r.Flush()
+	if err != nil {
+		return 0, err
 	}
-	if r.Available() <= 0 && r.Flush() != nil {
-		return r.err
+
+	// If b is bigger than buffer, write it directly
+	if len(b)-n > cap(r.buf) {
+		_, err = r.upstream.Write(stringToBytes(b[n:]))
+		return len(b), err
 	}
-	r.buf[r.n] = c
-	r.n++
-	return nil
+
+	// copy b to buffer
+	copy(r.buf, b[n:])
+	r.n = len(b) - n
+	return n, nil
 }
 
 func (r *Response) WriteInt(i int) (int, error) {
